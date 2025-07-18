10cbd514d71d62a56a534e97970e2d02006461b8
Janko94 <39459805+Janko94@users.noreply.github.com>
2025-07-18 18:54:47 +0200

    commit changes (#44)
    Co-authored-by: Janko Djukic <janko.djukic@tietoevry.com>

--- Characters/character_body_2d.gd
+++ Characters/character_body_2d.gd
@@ -1,25 +1,52 @@
 extends CharacterBody2D
 
+var speed: int = 200
+const normal_speed = 200
+const jump_speed: int = -400
+const dash_speed: int = 1200
+const dash_length: float = .05
 var current_ability1 : GlobalVariables.abilities
 var current_ability2 : GlobalVariables.abilities
 var current_ability3 : GlobalVariables.abilities
 @onready var interaction_area: Area2D = $InteractionArea
+@onready var playerDash = $PlayerDash
 
 func _process(delta: float) -> void:
+	velocity.x = 0
+	
+	if Input.is_action_just_pressed("ability3"):
+		dash()
+		playerDash.start_dash(dash_length)
+	speed = dash_speed if playerDash.is_dashing() else normal_speed
+	
+	if Input.is_action_pressed("ui_right"):
+		walk(true)
+	elif Input.is_action_pressed("ui_left"):
+		walk(false)
+	else:
+		# Only play idle if no special animation is active
+		if not $PlayerSprite.is_playing() or $PlayerSprite.animation == "walk":
+			$PlayerSprite.play("idle")
+	
 	if Input.is_action_just_pressed("ability1"):
 		jump()
+		
+	if Input.is_action_just_pressed("ability2"):
+		attack()
+		
+	if Input.is_action_just_pressed("ability3"):
+		dash()
 
 	move_and_slide()
 	velocity += get_gravity() * delta
-	for i in interaction_area.get_overlapping_bodies():
-		if i.is_in_group("interactable"):
-			i.show_interaction()
 
 func jump() -> void:
-	velocity.y = -500
+	velocity.y = jump_speed
+	$PlayerSprite.play("jump")
 	MessageBus.ability_used.emit(GlobalVariables.abilities.JUMP)
 
 func attack() -> void:
+	$PlayerSprite.play("damage")
 	MessageBus.ability_used.emit(GlobalVariables.abilities.ATTACK)
 
 func dash() -> void:
@@ -27,3 +54,11 @@ func dash() -> void:
 
 func interact() -> void:
 	MessageBus.ability_used.emit(GlobalVariables.abilities.INTERACT)
+
+func walk(is_direction_right: bool) -> void:
+	var direction = 1 if is_direction_right else -1
+	velocity.x = speed * direction
+	$PlayerSprite.flip_h = !is_direction_right
+	if not $PlayerSprite.animation == "dash":
+		$PlayerSprite.play("walk")
+	
